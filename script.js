f56b374485143d7176759d60ac8f4bf4b6a5ee0a
Heme106 <jakubhemerka12@gmail.com>
2026-03-01 20:51:01 +0100

    button

---
+++ script.js
@@ -0,0 +1,11 @@
+const button = document .getElementById("mech")
+
+
+button.addEventListener("click", () => {
+
+    const context = button .getElementById("mech-text")
+
+    context.classList.toggle("open")
+
+
+})
