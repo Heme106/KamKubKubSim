5f84999d6e79ee0ec8b1ff2db414f2c0d739b4a5
Heme106 <jakubhemerka12@gmail.com>
2026-03-01 22:44:04 +0100

    tlacitko2

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const button = document .getElementById("mech")
+const mech = document .getElementById("mech")
 const text = document .getElementById("text")
 
-button.addEventListener("click", function() {
+const typ = document .getElementById("typ")
+const text1 = document .getElementById("text1")
+
+mech.addEventListener("click", function() {
     text.classList.toggle("open")
+    
+})
+
+typ.addEventListener("click", function() {
+    text1.classList.toggle("open")
     
 })
