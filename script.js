5de48d4b582d3ae0c67c7bcc3dda549158f6c18f
Heme106 <jakubhemerka12@gmail.com>
2026-03-03 19:28:27 +0100

    Vývoj hry

--- script.js
+++ script.js
@@ -7,17 +7,21 @@ const text1 = document .getElementById("text1")
 const příb = document .getElementById("příb")
 const text2 = document .getElementById("text2")
 
+const výv = document .getElementById("výv")
+const text3 = document .getElementById("text3")
+
 mech.addEventListener("click", function() {
     text.classList.toggle("open")
-    
 })
 
 typ.addEventListener("click", function() {
-    text1.classList.toggle("open")
-    
+    text1.classList.toggle("open") 
 })
 
 příb.addEventListener("click", function() {
     text2.classList.toggle("open")
-    
+})
+
+výv.addEventListener("click", function() {
+    text3.classList.toggle("open")
 })
