2f03ef652cffd03c68ca72f40bd915beebd14a1c
Heme106 <jakubhemerka12@gmail.com>
2026-03-04 21:52:01 +0100

    pridani diagramu na tlacitko

--- script.js
+++ script.js
@@ -10,18 +10,35 @@ const text2 = document .getElementById("text2")
 const výv = document .getElementById("výv")
 const text3 = document .getElementById("text3")
 
-mech.addEventListener("click", function() {
+const dia = document .getElementById("dia")
+const diagram_vývoj = document .getElementById("diagram_vývoj")
+
+function otevrit_mechaniky () { 
     text.classList.toggle("open")
-})
+}
 
-typ.addEventListener("click", function() {
-    text1.classList.toggle("open") 
-})
+function otevrit_typy () { 
+    text1.classList.toggle("open")
+}
 
-příb.addEventListener("click", function() {
+function otevrit_pribeh () { 
     text2.classList.toggle("open")
-})
+}
 
-výv.addEventListener("click", function() {
+function otevrit_vyvoj () { 
     text3.classList.toggle("open")
-})
+}
+
+function otevrit_obrazek () { 
+    diagram_vývoj.classList.toggle("open")
+}
+
+mech.addEventListener("click", otevrit_mechaniky)
+
+typ.addEventListener("click", otevrit_typy)
+
+příb.addEventListener("click", otevrit_pribeh)
+
+výv.addEventListener("click", otevrit_vyvoj)
+
+dia.addEventListener("click", otevrit_obrazek)
